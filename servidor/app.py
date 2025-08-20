3a3d4e682c4762b814b45306e6f57a4923b57d11
Rocio <ralbarracintrab@gmail.com>
2025-08-20 11:39:38 -0300

    Insertamos registros sobre los datos recibidos del sensor

--- servidor/app.py
+++ servidor/app.py
@@ -16,7 +16,7 @@ def dict_factory(cursor, row):
 
 def abrirConexion():
   if 'db' not in g:
-     g.db = sqlite3.connect("api.sqlite")
+     g.db = sqlite3.connect("valores.sqlite")
      g.db.row_factory = dict_factory
   return g.db
 
@@ -29,14 +29,15 @@ def cerrarConexion(e=None):
 #No es aceptado por la pagina y por usamos el simulador "postman"
 @app.route('/api/sensor', methods = ['POST'])
 def sensor():
-    abrirConexion()
+    db = abrirConexion()
     nombre = request.json["nombre"] # "request.json": Usado para recibir el diccionario/JSON
     valor = request.json["valor"] # Usamos "request.json["valor"]" para acceder a un valor segun la clave indicada, la clave debe ser la que este en el JSON
     
     #Insertarlo en la base de datos
-    
+    db.execute("""INSERT INTO valores(nombre, valor) VALUES (?,?)""", (nombre,valor))
+    db.commit() #Cierra la transaccion
 
     print(f"nombre : {nombre} | valor : {valor}") #Esto se va ver en la terminal
-    cerrarConexion()
+    db = cerrarConexion()
 
     return "ok" #Esto se mando como response a la página
