4a88bbec9b3a0a0c225ef8bca6b2c3b31e3a3bd1
Rocio <ralbarracintrab@gmail.com>
2025-08-20 12:15:19 -0300

    Agregamos un ruta para el pagina y la lista de valores registrados

--- servidor/app.py
+++ servidor/app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, request, g
+from math import ceil
 #Necesitamos importa requesta para usarlo recibiendo pedidos
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ def dict_factory(cursor, row):
 
 def abrirConexion():
   if 'db' not in g:
-     g.db = sqlite3.connect("valores.sqlite")
+     g.db = sqlite3.connect("valores.sqlite") #Depende del archivo sqlite
      g.db.row_factory = dict_factory
   return g.db
 
@@ -41,3 +42,28 @@ def sensor():
     db = cerrarConexion()
 
     return {"Resultado": "ok"} #Mensaje jsonificado automaticamente, sin usar "jsonify"
+
+#Actividad 16
+resultados_por_pag = 2
+
+@app.route('/api/lista')
+def listaValores():
+   db = abrirConexion()
+
+   args = request.args
+   pagina = int(args.get('page', '1')) #Para reconocer el número de página en el que esta la persona (el "()" dice...)
+   descartar = (pagina-1) * resultados_por_pag #Al empezar en la página 1 es necesario restarle una pag para no descartar su misma cantidad y quedarnos sin página(si empezaramos a contar desde 0 no haria falta) 
+   db = abrirConexion()
+
+   #Para contar la cantidad de regique hay
+   cursor = db.cursor()
+   cursor.execute("SELECT COUNT(*) AS cant FROM artists;")
+
+   cant = cursor.fetchone()['cant'] #Devolvemos el resultado de la columna cant(creada cuando se realiza el COUNT)
+   paginas = ceil(cant / resultados_por_pag) #Responde a cuantas visualizaciones de registro van haber por página
+
+   if pagina < 1 or pagina > paginas:
+      return f"Página inexistente: {pagina}", 400
+   
+
+   db = cerrarConexion()
