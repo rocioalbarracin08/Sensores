3f80620023f8ff92891d01abac2892716df1230c
Rocio <ralbarracintrab@gmail.com>
2025-08-20 11:17:29 -0300

    Conexion con la base de datos (sin aun agregar registros a la base de datos)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Sensores
+
+Put: si hacemos 2 iguales, deberia quedar un mismo resultado
+POST: == , va ser cada uno un registro nuevo
--- servidor/app.py
+++ servidor/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+import sqlite3
+from flask import Flask, request, g
 #Necesitamos importa requesta para usarlo recibiendo pedidos
 
 app = Flask(__name__)
@@ -7,11 +8,35 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+def dict_factory(cursor, row):
+ """Arma un diccionario con los valores de la fila."""
+ fields = [column[0] for column in cursor.description]
+ return {key: value for key, value in zip(fields, row)}
+
+
+def abrirConexion():
+  if 'db' not in g:
+     g.db = sqlite3.connect("api.sqlite")
+     g.db.row_factory = dict_factory
+  return g.db
+
+
+def cerrarConexion(e=None):
+   db = g.pop('db', None)
+   if db is not None:
+       db.close()
+
 #No es aceptado por la pagina y por usamos el simulador "postman"
 @app.route('/api/sensor', methods = ['POST'])
 def sensor():
+    abrirConexion()
     nombre = request.json["nombre"] # "request.json": Usado para recibir el diccionario/JSON
     valor = request.json["valor"] # Usamos "request.json["valor"]" para acceder a un valor segun la clave indicada, la clave debe ser la que este en el JSON
+    
+    #Insertarlo en la base de datos
+    
 
     print(f"nombre : {nombre} | valor : {valor}") #Esto se va ver en la terminal
+    cerrarConexion()
+
     return "ok" #Esto se mando como response a la página
